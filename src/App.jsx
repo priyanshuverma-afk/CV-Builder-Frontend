092400a63e7bfe0802b635a6248b618ffd8a3b3c
priyanshuverma-afk <priyanshuverma81369@gmail.com>
2026-05-06 13:17:39 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-green-500 text-white p-6 rounded-xl shadow-lg">
+        <h1 className="text-2xl font-bold">
+          Tailwind React me working hai 🚀
+        </h1>
+        <p className="mt-2">
+          Agar yeh box green hai, sab sahi hai.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
